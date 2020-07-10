57235eab59ba29ddc86a73f9982e01ed96504d1c
Leonard Lam <tltheleno@gmail.com>
2020-07-10 02:06:15 -0700

    updated app path

--- public/portfolio/build/service-worker.js
+++ public/portfolio/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.161ee298a203674f30fcbd5f8e687c64.js"
+  "/portfolio/precache-manifest.abded5e46e2db3ead97635e773e5a2b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
